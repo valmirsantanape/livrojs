a98e1c55dad64da6b6c7b7dee193e9c6d6dfcffc
Valmir Santana <valmirsantanape@gmail.com>
2023-01-21 17:09:42 -0300

    lista de objetos - update

--- cap06/exemplos/ex04_node/lista-objetos.js
+++ cap06/exemplos/ex04_node/lista-objetos.js
@@ -1,7 +1,14 @@
 const carros = []
 carros.push({modelo: "sandero", preco: 46000}, {modelo: "palio" , preco: 39500}, {modelo: "crossfox", preco: 60000})
 
+const modelo = "celta"
+const preco = 26000
+
+//Quando o nome da variável é igual ao nome do atributo pode-se omitir a atribuição ao adicionar um novo objeto
+carros.push({modelo, preco})
+
+
 for(const element of carros){
-    console.log(element.modelo)
+    console.log(element.modelo,element.preco)
 }
 
