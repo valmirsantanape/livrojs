82eed46c9f0c0ea7fda989ad39b5f0ae4deb0fd5
Valmir Santana <valmirsantanape@gmail.com>
2023-01-21 00:39:58 -0300

    update

--- cap06/exemplos/ex2/script.js
+++ cap06/exemplos/ex2/script.js
@@ -21,6 +21,47 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
 })
 
-frm.btnUrgencia.addEventListener("click", (e) => {
+frm.btnUrgencia.addEventListener("click", () => {
+    if(!frm.checkValidity()){
+        alert("Informe o nome do paciente a ser atendido em carater de urgencia")
+        frm.inNome.focus()
+        return
+    }
+    const nome = frm.inNome.value
+    pacientes.unshift(nome)
+    let lista = ""
+    pacientes.forEach((paciente, i) => {
+        lista += `${i + 1}º ${paciente}\n`
+    })
+    respList.innerText = lista
+    frm.inNome.value = ""
+    frm.inNome.focus()
+})
+
+frm.btnAtender.addEventListener('click', (e) => {
+    
+    if (pacientes.length == 0) {
+        if(!frm.checkValidity()){
+           alert("Não tem paciente na fila de espera")
+            resp.innerText = ""
+            frm.inNome.focus()
+            return 
+        }else{
+            const emAtendimento = frm.inNome.value
+            resp.innerText = emAtendimento
+            frm.inNome.focus()
+            return
+        }
+        
+    }else{
+        const emAtendimento = pacientes.shift()
+        resp.innerText = emAtendimento
+        let lista = ""
+        pacientes.forEach((paciente, i) => {
+            lista += `${i + 1}º ${paciente}\n`
+        })
+        respList.innerText = lista
+    }
+    e.preventDefault()
     
 })
