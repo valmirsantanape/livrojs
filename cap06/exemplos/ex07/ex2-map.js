fe7c179caf68dc35a22ef4142ac2a708f40c6efc
Valmir Santana <valmirsantanape@gmail.com>
2023-01-30 16:41:18 -0300

    implemetando metodo filter

--- cap06/exemplos/ex07/ex2-map.js
+++ cap06/exemplos/ex07/ex2-map.js
@@ -1,6 +1,14 @@
-const amigos = [{nome: "Ana", idade: 20}, {nome: "Bruno", idade: 17}, {nome: "Catia", idade: 25}]
+const amigos = [
+    {nome: "Ana", idade: 20},
+    {nome: "Bruno", idade: 17},
+    {nome: "Thiago", idade: 30},
+    {nome: "João", idade: 23},
+    {nome: "Marcos", idade: 26}
+]
 
-const amigos2 = amigos.map(aux => ({nome: aux.nome, nasc: 2022 - aux.idade}))
+const amigos2 = amigos.map(aux => ({nome: aux.nome, nasc: 2022 - aux.idade, idade: aux.idade}))
+
+
 
 for(const amigo of amigos2){
     console.log(`${amigo.nome} - Nasceu em ${amigo.nasc}`)
--- cap06/exemplos/ex07/ex3-filter.js
+++ cap06/exemplos/ex07/ex3-filter.js
@@ -6,16 +6,16 @@ console.log(pares.join(','))
 console.log("=-".repeat(35))
 
 
-const amigos = [{nome: "Ana", idade: 20},
-                {nome: "Bruno", idade: 17},
-                {nome: "Catia", idade: 25}]
+const amigos = [
+    {nome: "Ana", idade: 20},
+    {nome: "Bruno", idade: 17},
+    {nome: "Thiago", idade: 30},
+    {nome: "João", idade: 16},
+    {nome: "Marcos", idade: 26}
+]
 
-const amigos2 = amigos.filter(aux => aux.idade >= 30 || aux.nome.includes("0"))
+const maiores = amigos.filter(aux => aux.idade >= 18)
 
-for(const amigo of amigos2){
-    console.log(`${amigo.nome} - Idade: ${amigo.idade} anos.`)
-
-}
-if(amigos2.length == 0){
-    console.log("Não há amigos que atenda essas condições na lista informada.")
+for( const amigo of maiores){
+    console.log(`Nome: ${amigo.nome} - Idade: ${amigo.idade}`)
 }
